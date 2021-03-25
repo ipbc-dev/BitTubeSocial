4b3e804f9daf2780871294e30ce7737116cdfe2e
Icabaleiro <cabaleiro8@gmail.com>
2021-03-25 13:10:34 +0000

    add bittubeapp to connectsrc

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -30,12 +30,12 @@ Rails.application.config.content_security_policy do |p|
   if Rails.env.development?
     webpacker_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{Webpacker.dev_server.host_with_port}" }
 
-    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls, 'https://europe-west1-bittube-airtime.cloudfunctions.net'
+    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls, 'https://bittubeapp.com','https://europe-west1-bittube-airtime.cloudfunctions.net'
     p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host, 'https://bittubeapp.com', 'https://cdn.jsdelivr.net'
     p.child_src   :self, :blob, assets_host
     p.worker_src  :self, :blob, assets_host
   else
-    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, 'https://europe-west1-bittube-airtime.cloudfunctions.net'
+    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, 'https://bittubeapp.com', 'https://europe-west1-bittube-airtime.cloudfunctions.net'
     p.script_src  :self, assets_host, 'https://bittubeapp.com', 'https://cdn.jsdelivr.net'
     p.child_src   :self, :blob, assets_host
     p.worker_src  :self, :blob, assets_host
